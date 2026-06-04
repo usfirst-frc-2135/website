a8ea23f8ff86768510f8068f084e85ef5510b8aa
Jeff Mullins <jeff.l.mullins@gmail.com>
2026-06-04 16:58:39 -0700

    Move slide locations

--- media_photos.php
+++ media_photos.php
@@ -11,59 +11,59 @@ require 'inc/header.php';
     <h1 class="fw-bold mt-3">Photos</h1>
     <hr>
     <div class="col-sm-3">
-      <a href="media_photos_2024-25.php"> <img src="img/home/slides/button_images/button24-25.jpg" width="180" height="130" alt="Photos24-25" /></a>
+      <a href="media_photos_2024-25.php"> <img src="img/media/slides/button_images/button24-25.jpg" width="180" height="130" alt="Photos24-25" /></a>
       <p></p>Photos 2024-25<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2023-24.php"> <img src="img/home/slides/button_images/button23-24.jpg" width="180" height="130" alt="Photos23-24" /></a>
+      <a href="media_photos_2023-24.php"> <img src="img/media/slides/button_images/button23-24.jpg" width="180" height="130" alt="Photos23-24" /></a>
       <p></p>Photos 2023-24<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2022-23.php"> <img src="img/home/slides/button_images/button22-23.jpg" width="180" height="130" alt="Photos22-23" /></a>
+      <a href="media_photos_2022-23.php"> <img src="img/media/slides/button_images/button22-23.jpg" width="180" height="130" alt="Photos22-23" /></a>
       <p></p>Photos 2022-23<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2021-22.php"> <img src="img/home/slides/button_images/button21-22.jpg" width="180" height="130" alt="Photos21-22" /></a>
+      <a href="media_photos_2021-22.php"> <img src="img/media/slides/button_images/button21-22.jpg" width="180" height="130" alt="Photos21-22" /></a>
       <p></p>Photos 2021-22<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2020-21.php"> <img src="img/home/slides/button_images/button20-21.jpg" width="180" height="130" alt="Photos20-21" /></a>
+      <a href="media_photos_2020-21.php"> <img src="img/media/slides/button_images/button20-21.jpg" width="180" height="130" alt="Photos20-21" /></a>
       <p></p>Photos 2020-21<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2019-20.php"> <img src="img/home/slides/button_images/button19-20.jpg" width="180" height="130" alt="Photos19-20" /></a>
+      <a href="media_photos_2019-20.php"> <img src="img/media/slides/button_images/button19-20.jpg" width="180" height="130" alt="Photos19-20" /></a>
       <p></p>Photos 2019-20<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2018-19.php"> <img src="img/home/slides/button_images/button18-19.jpg" width="180" height="130" alt="Photos18-19" /></a>
+      <a href="media_photos_2018-19.php"> <img src="img/media/slides/button_images/button18-19.jpg" width="180" height="130" alt="Photos18-19" /></a>
       <p></p>Photos 2018-19<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2017-18.php"> <img src="img/home/slides/button_images/button17-18.jpg" width="180" height="130" alt="Photos17-18" /></a>
+      <a href="media_photos_2017-18.php"> <img src="img/media/slides/button_images/button17-18.jpg" width="180" height="130" alt="Photos17-18" /></a>
       <p></p>Photos 2017-18<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2016-17.php"> <img src="img/home/slides/button_images/button16-17.jpg" width="180" height="130" alt="Photos16-17" /></a>
+      <a href="media_photos_2016-17.php"> <img src="img/media/slides/button_images/button16-17.jpg" width="180" height="130" alt="Photos16-17" /></a>
       <p></p>Photos 2016-17<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2015-16.php"> <img src="img/home/slides/button_images/button15-16.jpg" width="180" height="130" alt="Photos15-16" /></a>
+      <a href="media_photos_2015-16.php"> <img src="img/media/slides/button_images/button15-16.jpg" width="180" height="130" alt="Photos15-16" /></a>
       <p></p>Photos 2015-16<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2014-15.php"> <img src="img/home/slides/button_images/button14-15.jpg" width="180" height="130" alt="Photos14-15" /></a>
+      <a href="media_photos_2014-15.php"> <img src="img/media/slides/button_images/button14-15.jpg" width="180" height="130" alt="Photos14-15" /></a>
       <p></p>Photos 2014-15<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2013-14.php"> <img src="img/home/slides/button_images/button13-14.jpg" width="180" height="130" alt="Photos13-14" /></a>
+      <a href="media_photos_2013-14.php"> <img src="img/media/slides/button_images/button13-14.jpg" width="180" height="130" alt="Photos13-14" /></a>
       <p></p>Photos 2013-14<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2012-13.php"> <img src="img/home/slides/button_images/button12-13.jpg" width="180" height="130" alt="Photos12-13" /></a>
+      <a href="media_photos_2012-13.php"> <img src="img/media/slides/button_images/button12-13.jpg" width="180" height="130" alt="Photos12-13" /></a>
       <p></p>Photos 2012-13<p></p>
     </div>
     <div class="col-sm-3">
-      <a href="media_photos_2011-12.php"> <img src="img/home/slides/button_images/button11-12.jpg" width="180" height="130" alt="Photos11-12" /></a>
+      <a href="media_photos_2011-12.php"> <img src="img/media/slides/button_images/button11-12.jpg" width="180" height="130" alt="Photos11-12" /></a>
       <p></p>Photos 2011-12<p></p>
     </div>
 
