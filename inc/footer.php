b92e5ae5676fad760d5289391451ecd14fa8ad67
Jeff Mullins <jeff.l.mullins@gmail.com>
2026-06-18 23:02:42 -0700

    Fix footer layout

--- inc/footer.php
+++ inc/footer.php
@@ -4,31 +4,22 @@
   <!-- <div class="row"> -->
   <div class="row mx-auto">
     <div class="col-md-2"></div>
-    <div class="col-md-3 text-center">
-      <a class="row fs-6 text-white-50" href="https://www.firstinspires.org/">About FIRST</a>
-      <a class="row fs-6 text-white-50" href="https://www.firstinspires.org/frc">About FRC</a>
-      <a class="row fs-6 text-white-50" href="https://www.presentationhs.org/">About Presentation HS</a>
+    <div class="col-md-3">
+      <div><a class="text-white-50 text-start fs-6" href="https://www.firstinspires.org/">About FIRST</a></div>
+      <div><a class="text-white-50 text-start fs-6" href="https://www.firstinspires.org/frc">About FRC</a></div>
+      <div><a class="text-white-50 text-start fs-6" href="https://www.presentationhs.org/">About Presentation HS</a></div>
     </div>
-    <div class="col-md-3 text-center text-white-50">
-      <div class="row">
-        <div class="col-1">
-          <a href="https://www.google.com/maps/place/2281+Plummer+Ave,+San+Jose,+CA"><i class="bi bi-geo-alt text-white-50"></i></a>
-        </div>
-        <div class="col-10">
-          <p class="text-start">2281 Plummer Ave<br>San Jose, CA 95125<br>(408) 264-1664</p>
-        </div>
+    <div class="col-md-3">
+      <div>
+        <a class="text-white-50 text-start fs-6" href="https://www.google.com/maps/place/2281+Plummer+Ave,+San+Jose,+CA">
+          <i class="bi bi-geo-alt text-white-50 me-2"></i>
+          2281 Plummer Ave<br>San Jose, CA 95125<br>(408) 264-1664</a>
       </div>
     </div>
-    <div class="col-md-3 text-center">
-      <div class="row">
-        <a class="icon-link text-start fs-6 text-white-50" href="mailto:robotics@presentationhs.org"><i class="bi bi-envelope me-2"></i>Email Us</a>
-      </div>
-      <div class="row">
-        <a class="icon-link text-start fs-6 text-white-50" href="https://www.instagram.com/frc2135/"><i class="bi bi-instagram me-2"></i>Instagram</a>
-      </div>
-      <div class="row">
-        <a class="icon-link text-start fs-6 text-white-50" href="https://www.youtube.com/@Team2135"><i class="bi bi-youtube me-2"></i>YouTube</a>
-      </div>
+    <div class="col-md-3">
+      <div><a class="icon-link text-white-50 text-start fs-6" href="mailto:robotics@presentationhs.org"><i class="bi bi-envelope me-2"></i>Email Us</a></div>
+      <div><a class="icon-link text-white-50 text-start fs-6" href="https://www.instagram.com/frc2135/"><i class="bi bi-instagram me-2"></i>Instagram</a></div>
+      <div><a class="icon-link text-white-50 text-start fs-6" href="https://www.youtube.com/@Team2135"><i class="bi bi-youtube me-2"></i>YouTube</a></div>
     </div>
     <div class="col-md-1"></div>
     <!-- </div> -->
